dfc7de233f294c419ced8fb0baa8078a5e535efb
VladDev <vlad.iachim15@gmail.com>
2025-10-27 17:25:45 +0200

    Finnished entities

---
+++ src/main/java/com/example/Demo/entities/Comments.java
@@ -0,0 +1,37 @@
+package com.example.Demo.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "comments")
+public class Comments {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "task_id", nullable = false)
+    private Task task;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private Users user;
+
+    @Column(nullable = false)
+    private String content;
+
+    private LocalDateTime created_at;
+
+    @PrePersist
+    public void onCreate(){
+        this.created_at = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/com/example/Demo/entities/TaskAssignments.java
@@ -0,0 +1,26 @@
+package com.example.Demo.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "taskassignments")
+public class TaskAssignments {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "task_id", nullable = false)
+    private Task task;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private Users user;
+}
